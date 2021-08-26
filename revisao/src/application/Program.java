50429bd11ef8ceb82c23d1583e3a7167ca480714
Marcos <marcosviniciohd@hotmail.com>
2021-08-26 09:55:34 -0300

    Revisao introducao POO finalizada

--- revisao/src/application/Program.java
+++ revisao/src/application/Program.java
@@ -21,8 +21,25 @@ public class Program {
 		System.out.print("Quantidade: ");
 		produto.quantidade = sc.nextInt();
 		
+		System.out.println();
+		System.out.println("Dados do produto: " + produto);
 		
-		System.out.println(produto);
+		System.out.println();
+		System.out.print("Entre com número de produtos que deseja adicionar: ");
+		int quantidade = sc.nextInt();
+		produto.adicionarProduto(quantidade);
+		
+		System.out.println();
+		System.out.println("Dados do produto atualizado: " + produto);
+		
+		System.out.println();
+		System.out.print("Entre com número de produtos que deseja remover: ");
+		quantidade = sc.nextInt();
+		produto.removerProduto(quantidade);
+		
+		System.out.println();
+		System.out.println("Dados do produto: " + produto);
+		
 		
 		sc.close();
 		
